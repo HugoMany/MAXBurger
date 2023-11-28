67b7fde6eca5f0257d56fec572ff2aff0a6f36c7
HugoMany <90179566+HugoMany@users.noreply.github.com>
2023-11-28 14:44:09 +0100

    barman cuisinier personnel serveur

---
+++ Cuisinier.java
@@ -0,0 +1,3 @@
+public class Cuisinier extends Personnel {
+    
+}
---
+++ Personnel.java
@@ -0,0 +1,16 @@
+public class Personnel{
+    private String nom;
+    private String Prenom;
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public void setPrenom(String prenom) {
+        Prenom = prenom;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public String getPrenom() {
+        return Prenom;
+    }
+}
