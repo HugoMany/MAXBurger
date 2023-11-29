e4e747f8dc5c5852303e11593f1882b2f8cb0bc4
HugoMany <90179566+HugoMany@users.noreply.github.com>
2023-11-29 16:02:24 +0100

    Update Application.java

--- Application.java
+++ Application.java
@@ -2,6 +2,12 @@ import java.util.ArrayList;
 
 public class Application {
     protected ArrayList<Journee> listeDesJournée =new ArrayList<Journee>();
-    
 
+    public void addAJournee(Journee newJournee){
+        this.listeDesJournée.add(newJournee);
+    }
+    public ArrayList<Journee> getListeDesJournée() {
+        return listeDesJournée;
+    }
+    
 }
