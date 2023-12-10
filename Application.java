ea74b34b64c2dfb131bde0ade8f11c513aa98c18
Baptiste LIBERT <baptiste.libert@student.junia.com>
2023-12-10 13:24:28 +0100

    ajout addition menu principal

--- Application.java
+++ Application.java
@@ -559,7 +559,7 @@ public class Application {
         scanner.nextInt();
 
     }
-
+    
     public int getRandomNumber(int min, int max) {
         return (int) ((Math.random() * (max - min + 1)) + min);
     }
