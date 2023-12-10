899aa25adfe353c9f85a023a176d7ced970a90c6
HugoMany <90179566+HugoMany@users.noreply.github.com>
2023-12-10 11:59:26 +0100

    Stock UP

--- Ingredients.java
+++ Ingredients.java
@@ -7,4 +7,20 @@ public class Ingredients {
     public String toString() {
         return this.nom;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Ingredients other = (Ingredients) obj;
+
+        // Comparaison basée sur les noms
+        return nom.equals(other.nom);
+    }
 }
--- Stock.java
+++ Stock.java
@@ -1,26 +1,32 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Stock {
     public ArrayList<Ingredients> listDesIngredientsDispo = new ArrayList<Ingredients>();
 
-    
 
-    public void removeStock(String nameToRemove) {
-        for (int i = 0; i < this.listDesIngredientsDispo.size(); i++) {
-            if (listDesIngredientsDispo.get(i).nom == nameToRemove) {
-                this.listDesIngredientsDispo.remove(i);
-            }
-        }
-    }
     public void removeStock(ArrayList<Ingredients> listDesIngredientsToRemove) {
-        for (int i = 0; i < this.listDesIngredientsDispo.size(); i++) {
-            for (int j = 0; j < listDesIngredientsToRemove.size(); j++) {
-                if (listDesIngredientsDispo.get(i).nom == listDesIngredientsToRemove.get(j).nom) {
-                    listDesIngredientsDispo.remove(i);
+        for (Ingredients ingredientToRemove : listDesIngredientsToRemove) {
+            boolean removed = false;
+
+            Iterator<Ingredients> iterator = this.listDesIngredientsDispo.iterator();
+
+            while (iterator.hasNext()) {
+                Ingredients ingredient = iterator.next();
+
+                if (ingredient.equals(ingredientToRemove)) {
+                    iterator.remove();
+                    removed = true;
+                    break;  // Arrêtez la boucle après avoir trouvé et supprimé le premier élément
                 }
             }
+
+            if (!removed) {
+                System.out.println("Les "+ingredientToRemove.nom + " ont été supprimer du stock.");
+            }
         }
     }
+
     public void addStock(Ingredients ingredientToAdd) {
         this.listDesIngredientsDispo.add(ingredientToAdd);
     }
